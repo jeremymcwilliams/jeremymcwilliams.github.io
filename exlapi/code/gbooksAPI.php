72d3560963628fcd965c41287260263cd0b4c4c3
jeremymcwilliams <jeremym@lclark.edu>
2015-09-10 14:42:47 -0700

    xx

--- exlapi/code/gbooksAPI.php
+++ exlapi/code/gbooksAPI.php
@@ -1,21 +1,22 @@
-<?php
-	
+<?php	
 	$isbn="9781592968503";
-	$isbn="9781564581792";
 	
+	/* construct my REST request URL */
 	$url = "https://www.googleapis.com/books/v1/volumes?q=isbn:$isbn";
 	
+	/* query the API */
 	$json = file_get_contents($url);
 	
+	/* Turns JSON data into an "object" that PHP can use */
 	$data = json_decode($json);
-	
-	var_dump($data);
-	
+
+	/* Targets the "thumbnail" field in the response, and creates 
+	variable "$image" that contains the thumbnail value */	
 	$image=$data->items[0]->volumeInfo->imageLinks->thumbnail;
 	
-	echo $image;
-
-
-
-
+	/* create $html variable with HTML to display image */
+	$html="<img src='$image'>";
+	
+	/* outputs the image */
+	echo $html;
 ?>
